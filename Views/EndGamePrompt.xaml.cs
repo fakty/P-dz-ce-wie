40e7463319992d321029d073141feffa47a4ad90
fakty <michu.prz@interia.pl>
2016-10-13 20:43:28 +0200

    EndGameWindow - creating

---
+++ Views/EndGamePrompt.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Windows.Input;
+
+namespace Pędzące_Żółwie
+{
+    /// <summary>
+    /// Logika interakcji dla klasy EndGamePrompt.xaml
+    /// </summary>
+    public partial class EndGamePrompt
+    {
+        public EndGamePrompt()
+        {
+            InitializeComponent();
+            Cursor = new Cursor(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Resources\\green.cur"));
+        }
+    }
+}
